8cacec61a126048e9c419adc8efb2c0cc5803a42
ReaksaChan <chanreaksa.chhim@mjqeducation.edu.kh>
2025-02-04 11:13:03 +0700

    add component

---
+++ src/components/Header.tsx
@@ -0,0 +1,16 @@
+
+const Header = () => {
+  return (
+    <>
+        <h1 className="text-primary">Welcome to My Website!</h1>
+
+        <nav>
+            <a href="">Home</a>
+            <a href="">About</a>
+            <a href="">Contact</a>
+        </nav>
+    </>
+  )
+}
+
+export default Header;
---
+++ src/components/Main.tsx
@@ -0,0 +1,11 @@
+
+const Main = () => {
+  return (
+    <main>
+        <h2>Main Context</h2>
+        <p>This is the main context</p>
+    </main>
+  )
+}
+
+export default Main
